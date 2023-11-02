4056dfda3c45d94867ae9c6af7f6593962abc3b6
Apronius <arcg51@gmail.com>
2023-11-01 22:09:15 -0400

    rectangle moves now

--- snake.py
+++ snake.py
@@ -1,20 +1,25 @@
 import pygame
 
 class Piece():
-    def __init__(self):
-        self.pos = None
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(x, y, 50, 50)
         self.prev = None
         self.next = None
         
+    def pickUp(self, snake):
+        if(snake.head.x == self.x and snake.head.y == self.y):
+            snake.addPiece()
+            
+        
 class Snake():
     def __init__(self):
-        self.pieces = [self.head]
-        self.head = Piece()
-        self.tail = Piece()
-        self.head.next = None
+        self.head = Piece(200, 200)
+        self.tail = Piece(200, 200)
         self.head.prev = self.tail
         self.tail.next = self.head
-        self.head.pos = (200, 200)
+        self.pieces = [self.head]
         self.length = 0
         
     def addPiece(self):
@@ -31,8 +36,10 @@ class Snake():
         self.length += 1
         
     def collision(self) -> bool:
+        if(self.length == 0):
+            return False
         for piece in self.pieces[1:]:
-            if piece.pos == self.pieces[0].pos:
+            if piece.x == self.pieces[0].x and piece.y == self.pieces[0].y:
                 return True
         return False
     
@@ -47,9 +54,11 @@ def main():
     clock = pygame.time.Clock()
     running = True
     dt = 0
-
-    rec = pygame.Rect(posX, posY, 50, 50) 
+    snake = Snake()
+    direction = 0
+    
     while running:
+        
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
@@ -59,20 +68,32 @@ def main():
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("white")
 
-        pygame.draw.rect(screen, "red", rec)
-        #pygame.draw.rect(screen, "blue", screen.get_width() / 2, screen.get_height() / 2,20,20, 0)
+        pygame.draw.rect(screen, "red", snake.head.rect)
 
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
-            posY -= 300 * dt
-        if keys[pygame.K_s]:
-            posY += 300 * dt
-        if keys[pygame.K_a]:
-            posX -= 300 * dt
-        if keys[pygame.K_d]:
-            posX += 300 * dt
-
+            direction = 0
+        elif keys[pygame.K_s]:
+            direction = 1
+        elif keys[pygame.K_a]:
+            direction = 2
+        elif keys[pygame.K_d]:
+            direction = 3
+        
+        match direction:
+            case 0:
+                snake.head.rect.move_ip(0, -500 * dt)
+            case 1:
+                snake.head.rect.move_ip(0, 500 * dt)
+            case 2:
+                snake.head.rect.move_ip(-500 * dt, 0)
+            case 3:
+                snake.head.rect.move_ip(500 * dt, 0)
+        
+        if(snake.collision()):
+            print("Oops! Bad!")
+            running = False
 
         # flip() the display to put your work on screen
         pygame.display.flip()
