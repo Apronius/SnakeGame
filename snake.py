3891e5d5aff49e9d9d0e07f683897a57773e4140
Apronius <arcg51@gmail.com>
2023-11-28 18:46:17 -0500

    Revamped snake game, uses board now, no snake obj

--- snake.py
+++ snake.py
@@ -1,70 +1,63 @@
 import pygame
 import random
 
-class Piece():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.rect = pygame.Rect(x, y, 50, 50)
-        self.prev = None
-        self.next = None
-        
-    def pickUp(self, snake) -> bool:
-        if((snake.head.x >= self.x and (snake.head.x +50) <= self.x ) and (snake.head.y >= self.y and (snake.head.y + 50) <= self.y)):
-            print("IN")
-            snake.addPiece()
-            return True
-        return False
-            
-        
-class Snake():
-    def __init__(self):
-        self.head = Piece(200, 200)
-        self.tail = Piece(200, 200)
-        self.head.prev = self.tail
-        self.tail.next = self.head
-        self.pieces = [self.head]
-        self.length = 0
-        
-    def addPiece(self):
-        piece = Piece(self.tail.next.x-50, self.tail.next.y-50)
-        if self.length == 0:
-            piece.next = self.head
-            self.tail.next = piece
-            self.head.prev = piece
-        else:
-            piece.next = self.tail.next
-            self.tail.next.prev = piece
-            self.tail.next = piece
-        self.pieces.append(piece)
-        self.length += 1
-        
-    def collision(self) -> bool:
-        if(self.length == 0):
-            return False
-        for piece in self.pieces[1:]:
-            if piece.x == self.pieces[0].x and piece.y == self.pieces[0].y:
-                return True
-        return False
-    
-    def length(self) -> int:
-        return self.length
+X_SPACES: int = 25
+Y_SPACES: int = 14
 
+PIECE_SIZE: int = 35
+
+class Board():
+    board = []
+    
+    def __init__(self):
+        self.board = [[pygame.Rect(50,50,PIECE_SIZE,PIECE_SIZE)]*X_SPACES]*Y_SPACES 
+        print(f"row: {len(self.board)} column: {len(self.board[0])}")      
+         
+    def populateBoard(self):
+        print(f"len board = {len(self.board)} len inner = {len(self.board[0])}")
+        for r in range(Y_SPACES):
+            for c in range(X_SPACES):
+                self.board[r][c] = pygame.Rect(50*(r+1), 50*(c+1), PIECE_SIZE, PIECE_SIZE)
+                
+    def newPieceLocation(self, pieces):
+        newPieceX = random.randint(X_SPACES)
+        newPieceY = random.randint(Y_SPACES)
+        for (x, y) in pieces:
+            if(x == newPieceX and y == newPieceY):
+                newPieceX = random.randint(X_SPACES)
+                newPieceY = random.randint(Y_SPACES)
+        return (newPieceX, newPieceY)
+            
+    def drawBoard(self, screen, pieces, newPiece):
+        # for i in range(len(pieces)):
+        #     pygame.draw.rect(screen, "red", self.board[pieces[i][0]][pieces[i][1]])
+        # pygame.draw.rect(screen, "green", self.board[newPiece[0]][newPiece[1]])
+        for i in range(Y_SPACES):
+            for j in range(X_SPACES):
+                pygame.draw.rect(screen, ("red" if j%2==0 else "green") if i%2==0 else ("green" if j%2==0 else "red"), pygame.Rect(50*(j+1), 50*(i+1), PIECE_SIZE, PIECE_SIZE))
         
+    def collision(self, pieces, newPiece):
+        for piece in pieces:
+            if(piece[0] == newPiece[0] and piece[1] == newPiece[1]):
+                 
+         
+         
 def main():
     # pygame setup
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((1400, 800))
     clock = pygame.time.Clock()
     running = True
     dt = 0
-    snake = Snake()
+
     direction = 0
-    x = random.randint(0, 1280 - 50)
-    y = random.randint(0, 720 - 50)
     
+    board = Board()
+    board.populateBoard()
+
+    newPiece = [5, 5]
+
     while running:
-        
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
@@ -73,15 +66,10 @@ def main():
 
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("white")
+        
+        pieces = [[0, 0]]
 
-        for piece in snake.pieces:
-            pygame.draw.rect(screen, "red", piece.rect)
-
-        piece = Piece(x, y)
-        if(piece.pickUp(snake)):
-            x = random.randint(0, 1280 - 50)
-            y = random.randint(0, 720 - 50)
-        pygame.draw.rect(screen, "green", piece.rect)
+        board.drawBoard(screen, pieces, newPiece)
         
         keys = pygame.key.get_pressed()
 
@@ -103,10 +91,6 @@ def main():
                 snake.head.rect.move_ip(-500 * dt, 0)
             case 3:
                 snake.head.rect.move_ip(500 * dt, 0)
-        
-        if(snake.collision()):
-            print("Oops! Bad!")
-            running = False
 
         # flip() the display to put your work on screen
         pygame.display.flip()
